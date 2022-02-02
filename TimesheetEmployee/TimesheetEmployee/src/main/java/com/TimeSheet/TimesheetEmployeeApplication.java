221b501254a5f950ac31b3325af150c4593a00dc
komalgavkhar <komal.gawakhare@capgemini.com>
2022-02-02 10:02:38 +0530

    Create TimesheetEmployeeApplication.java

---
+++ TimesheetEmployee/TimesheetEmployee/src/main/java/com/TimeSheet/TimesheetEmployeeApplication.java
@@ -0,0 +1,13 @@
+package com.TimeSheet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TimesheetEmployeeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TimesheetEmployeeApplication.class, args);
+	}
+
+}
