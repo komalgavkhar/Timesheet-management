7b916448d61bf49c539fdf343e11ad3a562f21f9
komalgavkhar <komal.gawakhare@capgemini.com>
2022-02-02 10:02:24 +0530

    Create RecordNotFountException.java

---
+++ TimesheetEmployee/TimesheetEmployee/src/main/java/com/TimeSheet/Exception/RecordNotFountException.java
@@ -0,0 +1,12 @@
+package com.TimeSheet.Exception;
+
+@SuppressWarnings("serial")
+public class RecordNotFountException extends RuntimeException{
+	//private static final long serialversionUID=1L;
+
+	public RecordNotFountException(String msg) {
+		super(msg);
+	}
+
+
+}
